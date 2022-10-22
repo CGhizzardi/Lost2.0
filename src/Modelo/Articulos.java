11076bf8710e91c3c09cb69cb6634316b3415cc9
sebas2santos2206 <sdossanto@uoc.edu>
2022-10-22 18:20:43 +0200

    Esta es mi parte es completamente funcional

--- src/Modelo/Articulos.java
+++ src/Modelo/Articulos.java
@@ -5,58 +5,72 @@ package Modelo;
 import java.sql.Time;
 import java.util.Date;
 
-    public class Articulos {
-
-        /**
-         * Atributos de la clase
-         */
-
-        private String codigo;
-        private String descripcion;
-        private double precioDeVenta;
-        private double gastosDeEnvio;
-        private int tiempoDePreparacion;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
 
-        public Articulos(String codigo, String descripcion, double precioDeVenta, double gastoDeEnvio, int tiempoDePrepar) {
-            this.codigo = codigo;
-            this.descripcion = descripcion;
-            this.precioDeVenta = precioDeVenta;
-            this.gastosDeEnvio = gastoDeEnvio;
-            this.tiempoDePreparacion = tiempoDePrepar;
 
-        }
-        /** Metodos getters y setters */
+public class Articulos {
+    private String codigo;
+    private String descripcion;
+    private double precioDeVenta;
+    private double gastosDeEnvio;
+    private int tiempoDePreparacion;
 
-        public String getCodigo() {
-            return codigo;
-        }
-
-        public void setCodigo(String codigo) {
-            this.codigo = codigo;
-        }
-
-        public double getPrecioDeVenta() {
-            return precioDeVenta;
-        }
-
-        public void setPrecioDeVenta(double precioDeVenta) {
-            this.precioDeVenta = precioDeVenta;
-        }
-
-        public double getGastosDeEnvio() {
-            return gastosDeEnvio;
-        }
-
-        public void setGastosDeEnvio(double gastosDeEnvio) {
-            this.gastosDeEnvio = gastosDeEnvio;
-        }
-
-        public int getTiempoDePreparacion() {
-            return tiempoDePreparacion;
-        }
-
-        public void setTiempoDePreparacion(int tiempoDePreparacion) {
-            this.tiempoDePreparacion = tiempoDePreparacion;
-        }
+    public Articulos(String codigo, String descripcion, double precioDeVenta, double gastoDeEnvio, int tiempoDePrepar) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precioDeVenta = precioDeVenta;
+        this.gastosDeEnvio = gastoDeEnvio;
+        this.tiempoDePreparacion = tiempoDePrepar;
     }
+
+    public Articulos() {
+    }
+
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPrecioDeVenta() {
+        return this.precioDeVenta;
+    }
+
+    public void setPrecioDeVenta(double precioDeVenta) {
+        this.precioDeVenta = precioDeVenta;
+    }
+
+    public double getGastosDeEnvio() {
+        return this.gastosDeEnvio;
+    }
+
+    public void setGastosDeEnvio(double gastosDeEnvio) {
+        this.gastosDeEnvio = gastosDeEnvio;
+    }
+
+    public int getTiempoDePreparacion() {
+        return this.tiempoDePreparacion;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setTiempoDePreparacion(int tiempoDePreparacion) {
+        this.tiempoDePreparacion = tiempoDePreparacion;
+    }
+
+    public String toString() {
+        return "articulos{codigo='" + this.codigo + "', descripcion='" + this.descripcion + "', precioDeVenta=" + this.precioDeVenta + ", gastosDeEnvio=" + this.gastosDeEnvio + ", tiempoDePreparacion=" + this.tiempoDePreparacion + "}";
+    }
+}
