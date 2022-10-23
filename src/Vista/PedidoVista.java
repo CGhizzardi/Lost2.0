b96eef8f42c95fec267b2bca9e008f651afd3e93
Dídac <107183107+dtarragol@users.noreply.github.com>
2022-10-23 07:46:07 +0200

    vistaPedido

--- src/Vista/PedidoVista.java
+++ src/Vista/PedidoVista.java
@@ -1,4 +1,10 @@
 package Vista;
 
+import java.time.LocalDateTime;
+
 public class PedidoVista {
+    public void printPedido (Object pedido) {
+        System.out.println(pedido.toString());
+
+    }
 }
