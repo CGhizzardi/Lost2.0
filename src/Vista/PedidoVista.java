0cfa90f818179f3730d6f91cefb5462b10129f58
Dídac <107183107+dtarragol@users.noreply.github.com>
2022-10-28 20:55:27 +0200

    Update PedidoVista.java

--- src/Vista/PedidoVista.java
+++ src/Vista/PedidoVista.java
@@ -3,14 +3,7 @@ import Controlador.*;
 
 public class PedidoVista {
 
-    //public void printPedido (Object pedido) {
-    //    System.out.println(pedido.toString());
-    //}
-
-    // tal y como esta en el ejemplo:
     public void imprimirPedido (controlPedidos pedido){
-
-
         System.out.println("numeroPedido=" + pedido.getNumeroPedido() +
                 ", fechaHora=" + pedido.getFechaHora() +
                 ", Nif del cliente=" + pedido.getCliente().getNif() +
