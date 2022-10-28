e85d718627c8472067cf9a4cf030174592fbfb77
Dídac <107183107+dtarragol@users.noreply.github.com>
2022-10-28 21:00:50 +0200

    Update PedidoVista.java

--- src/Vista/PedidoVista.java
+++ src/Vista/PedidoVista.java
@@ -4,17 +4,17 @@ import Controlador.*;
 public class PedidoVista {
 
     public void imprimirPedido (controlPedidos pedido){
-        System.out.println("numeroPedido=" + pedido.getNumeroPedido() +
-                ", fechaHora=" + pedido.getFechaHora() +
-                ", Nif del cliente=" + pedido.getCliente().getNif() +
-                ", Nombre del Cliente=" + pedido.getCliente().getNombre() +
-                ", Articulo codigo=" + pedido.getArticulo().getCodigo() +
-                ", Descripción=" + pedido.getArticulo().getDescripcion() +
-                ", Cantidad=" + pedido.getCantidadArticulos() +
-                ", Articulo codigo=" + pedido.getArticulo().getPrecioDeVenta() +
-                ", Envio=" + pedido.getPrecioTotal() +" €"+
-                ", Precio total=" + pedido.getPrecioTotal() + " €"+
-                ", El pedido esta enviado=" + pedido.getEnviado() +
+        System.out.println("numeroPedido= " + pedido.getNumeroPedido() +
+                ", fechaHora= " + pedido.getFechaHora() +
+                ", Nif del cliente= " + pedido.getCliente().getNif() +
+                ", Nombre del Cliente= " + pedido.getCliente().getNombre() +
+                ", Articulo codigo= " + pedido.getArticulo().getCodigo() +
+                ", Descripción= " + pedido.getArticulo().getDescripcion() +
+                ", Cantidad= " + pedido.getCantidadArticulos() +
+                ", Articulo codigo= " + pedido.getArticulo().getPrecioDeVenta() +
+                ", Envio= " + pedido.getPrecioTotal() +" €"+
+                ", Precio total= " + pedido.getPrecioTotal() + " €"+
+                ", El pedido esta enviado= " + pedido.getEnviado() +
                 '}');
     }
 }
