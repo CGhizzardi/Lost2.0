2f58c3ca3cbf85d7bcb58034317ee5406bfadb6d
Carlo UOC <114773600+CGhizzardi@users.noreply.github.com>
2022-10-30 20:00:08 +0100

    Online Store

--- src/Vista/ArticuloVista.java
+++ src/Vista/ArticuloVista.java
@@ -4,9 +4,11 @@ import Controlador.controller;
 import Controlador.controlArticulos;
 import Vista.GestionOs.*;
 
+import java.util.Scanner;
+
 
 public class ArticuloVista {
-
+Scanner input= new Scanner(System.in);
     protected controlArticulos contArt;
     protected controller cont;
 
@@ -20,7 +22,48 @@ public class ArticuloVista {
 
         this.contArt=contArt;
 
+
     }
+
+            public void menuArticulos() {
+            boolean salir = false;
+            char opcio;
+            do {
+                System.out.println("1. Añadir Cliente");
+                System.out.println("2. Gestión Clientes");
+                System.out.println("0. Salir");
+                opcio = pedirOpcion();
+                switch (opcio) {
+                    case '1':
+
+
+// TO-BE-DONE
+                        break;
+                    case '2':
+                        System.out.println("Imprimir Articulo");
+                        cont.MostrarArticulo();
+                        break;
+                    case '0':
+                        salir = true;
+                }
+            } while (!salir);
+        }
+        char pedirOpcion() {
+            String resp;
+            System.out.println("Elige una opción (1,2,3 o 0):");
+            resp = input.nextLine();
+            if (resp.isEmpty()) {
+                resp = " ";
+            }
+            return resp.charAt(0);
+        }
+
+
+
+
+
+
+
 
 
     /** El metodo para imprimir los datos del articulo */
