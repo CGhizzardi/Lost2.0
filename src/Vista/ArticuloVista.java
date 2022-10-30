4c842afe79f8cc3e928a5ec94df0a14fcc083275
sebas2santos2206 <sdossanto@uoc.edu>
2022-10-30 12:53:53 +0100

    cuando puedas cesar o didac respondeme que tegno  unas preguntas sobre las listas, gracias

--- src/Vista/ArticuloVista.java
+++ src/Vista/ArticuloVista.java
@@ -1,9 +1,16 @@
 package Vista;
 import Controlador.controlArticulos;
+import Modelo.Articulos;
+import Modelo.ListaArticulos;
+
+import java.util.Scanner;
 
 public class ArticuloVista {
 
-    protected controlArticulos contArt;
+    public controlArticulos contArt;
+    Scanner teclado = new Scanner(System.in);
+
+
 
 
     /** Metodo constructor de la clase
@@ -11,13 +18,40 @@ public class ArticuloVista {
      * @param contArt
      */
 
-    public ArticuloVista(controlArticulos contArt){
-
-        this.contArt=contArt;
-
+    public ArticuloVista(controlArticulos contArt) {
+        this.contArt = contArt;
     }
 
+    public void gestionarticulos() {
+        boolean salir = false;
+        char opcio;
+        do {
+            System.out.println("1. Añadir articulos");
+            System.out.println("2. Mostrar articulos");
+            System.out.println("0. Salir");
+            opcio = pedirOpcion();
+            switch (opcio) {
+                case '1':
+// TO-BE-DONE
+                    break;
+                case '2':
+// TO-BE-DONE
 
+                    break;
+                case '0':
+                    salir = true;
+            }
+        } while (!salir);
+    }
+    char pedirOpcion() {
+        String resp;
+        System.out.println("Elige una opción (1,2 o 0):");
+        resp = teclado.nextLine();
+        if (resp.isEmpty()) {
+            resp = " ";
+        }
+        return resp.charAt(0);
+    }
     /** El metodo para imprimir los datos del articulo
      *
      * @param contArt
